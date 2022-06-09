2c5e651703ff4c8bb9daf739687cd841483e238d
Kyle Richnafsky <7krich@gmail.com>
2022-06-09 14:48:06 -0400

    add new friend & remove friend logic added

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -38,6 +38,48 @@ const userController = {
         User.create(body)
         .then(dbUserData => res.json(dbUserData))
         .catch(err => res.status(400).json(err))
+    },
+
+    // update User
+    updateUser({ params, body }, res) {
+        // find single docuemnt to update, update it, & return orig doc
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbUserData => {
+            if (!dbUserData) {
+            res.status(404).json({ message: 'No User found with this id!' });
+            return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    // delete User
+    deleteUser({ params }, res) {
+        // find document & delete it from db
+        User.findOneAndDelete({ _id: params.id })
+        .then(dbUserData => {
+            if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    // add friend
+    newFriend({ params }, res) {
+        User.findOneAndUpdate({ _id: params.userId }, { $push: {friends: params.friendId }}, { new: true, runValidators: true })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err))
+    },
+
+    // remove friend
+    removeFriend({ params }, res) {
+        User.findOneAndUpdate({ _id: params.userId }, { $pull : { friends: params.friendsId }})
+        .then(dbUserData => res.json(dbUserData, (params.friendId, 'User')))
+        .catch(err => res.json(err))
     }
 };
 
