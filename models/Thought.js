8c0d88cda47cbb11a0cf2bc4f6c571e697fe863a
Kyle Richnafsky <7krich@gmail.com>
2022-06-09 13:23:16 -0400

    created thought & reaction Schemas

---
+++ models/Thought.js
@@ -0,0 +1,67 @@
+const { Schema, model, Types } = require('mongoose');
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+           type: Date,
+           default: Date.now,
+           get: (time) => format_date(time) 
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [
+            reactionSchema
+        ]
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            deafult: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema)
+
+module.exports = Thought
