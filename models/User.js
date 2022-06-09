5aa2b0cb5e0090ae7135597dc106efa3291db52c
Kyle Richnafsky <7krich@gmail.com>
2022-06-09 11:55:05 -0400

    set up User model, file structure & server.js

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model, Types } = require('mongoose');
+
+const UserSchema = new Schema (
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+        },
+        // array of id values refrencing Thought model
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+// create User model
+const User = model('User', UserSchema);
+
+// export the User model
+module.exports = User;
